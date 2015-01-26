7399bab466aebef170c8995083796ff74bf4f04e
zdespolit <zdespolit@gmail.com>
2015-01-26 07:35:37 +0500

    Added tabs.

--- app.js
+++ app.js
@@ -70,11 +70,26 @@
   var links = []; // links cache
 
   angular
-      .module('demoApp', ['ui.tree'])
-      .controller('TwoTreesCtrl', function($scope) {
-        // the controller manages the creation of links
+      .module('demoApp', ['ui.tree', 'ui.bootstrap'])
+      .controller('MainCtrl', function($scope) {
 
-        $scope.list = data; // assign data into the scope
+        $scope.tabsets = [{id: 'left'}, {id: 'right'}];
+        
+        $scope.trees =  [{
+          id: '0',
+          name: 'first',
+          list: data
+        },{
+          id: '1',
+          name: 'second',
+          list: data.slice(0, 1)
+        },{
+          id: '2',
+          name: 'third',
+          list: data.slice(0, 2)
+        }];
+
+        $scope._leftTree = $scope._rightTree = $scope.trees[0].id;
 
         $scope.newSubItem = function(scope) {
           var nodeData = scope.$modelValue;
@@ -83,110 +98,178 @@
             title: nodeData.title + '.' + (nodeData.nodes.length + 1),
             nodes: []
           });
-          redrawLines();
+          $scope.redrawLines();
         };
 
+        /**
+         * Connect nodes from different tabsets
+         */
         $scope.connect = function(scope){
           var $el = scope.$element;
           $el.toggleClass('selected');
 
-          var parentId = $el.parents('.tree').data('id'); // parentId is an id of a tree, I use 'left' and 'right' in this demo
+          var parentId = $scope.getTabsetId($el);
           $scope[parentId] = $el.hasClass('selected') ? $el : null; // remember the selected element with respect to the tree containing the element
 
-          var left = $scope['left'];
-          var right = $scope['right'];
+          var left = $scope['_left'],
+              right = $scope['_right'];
+
           if (left && right){ // if there are selected items in both trees
-            if (!findLink(left, right)){ // and there is no link between the items
-              addLink(left, right); // then create it
-              drawLine(left, right); // and draw
+            var leftTree = getTree(left),
+                rightTree = getTree(right),
+                leftId = id(leftTree, left),
+                rightId = id(rightTree, right);
+
+            if (!findLink(leftId, rightId)){ // and there is no link between the items
+              addLink({
+                first: left,
+                firstId: leftId,
+                second: right,
+                secondId: rightId
+              });
+              drawLine(left, right, false); // and draw
             }
             $scope.resetConnecting(); // and clear state
           }
           return true;
         };
 
+        /**
+         * Clear the state if connection of nodes is aborted
+         */
         $scope.resetConnecting = function(){
-          var left = $scope['left'];
-          var right = $scope['right'];
+          var left = $scope['_left'];
+          var right = $scope['_right'];
           left && left.toggleClass('selected');
           right && right.toggleClass('selected');
-          $scope['left'] = null;
-          $scope['right'] = null;
+          $scope['_left'] = null;
+          $scope['_right'] = null;
         };
 
-        // this is set via ui-tree attribute
+        /**
+         * Options of the tree which are via ui-tree attribute
+         * @type {{dragStop: Function, removed: Function}}
+         */
         $scope.treeOptions = {
           dragStop: function(){
-            redrawLines();
+            $scope.redrawLines();
             $scope.resetConnecting();
           },
           removed: function(scope){
-            removeLinks(scope.$element.children('.angular-ui-tree-handle:first'));
-            redrawLines();
+            var $el = scope.$element.children('.angular-ui-tree-handle:first'),
+                $tree = getTree($el);
+            removeLinks(id($tree, $el));
+            $scope.redrawLines();
           }
         };
 
+        /**
+         * Redraw lines after toggling visibility of child nodes
+         */
         $scope.afterToggle = function(){
-          redrawLines();
-        }
+          $scope.redrawLines();
+        };
+
+        /**
+         * Remove the lines and show only relative ones
+         */
+        $scope.redrawLines = function(){
+          setTimeout(function(){ // async to let all the dom changes to take effect
+            $('.line').remove();
+            $('.circle').remove();
+            var l = links.length, link;
+            while (l-- > 0){
+              link = links[l];
+              if ($scope.shouldShowLink(link)){
+                drawLine(getNode(link.firstId, 'left'), getNode(link.secondId, 'right'), true);
+              }
+            }
+          }) 
+        };
+
+        /**
+         * Find if the link nodes belong two visible trees
+         */
+        $scope.shouldShowLink = function(link){
+          return link
+              && treeVisible(link.firstId, '_left')
+              && treeVisible(link.secondId, '_right');
+
+          function treeVisible(id, tabset){
+            return $scope[tabset + 'Tree'] == id.split(':')[0];
+          }
+        };
+
+        /**
+         * Change active tab/tree
+         * @param index tab index
+         * @param tabsetId
+         */
+        $scope.select = function(index, tabsetId) {
+          $scope['_' + tabsetId + 'Tree'] = this.tree.id;
+          $scope.redrawLines();
+        };
+
+        $scope.getTabsetId = function($el){
+          return '_' + $el.parents('.tabset').attr('id').replace('_tabset','')
+        };
+
+        /**
+         * Redraw on resize
+         */
+        $(window).on('resize', function(){
+          $scope.redrawLines();
+        });
 
       });
 
-  /**
-   * Redraw on resize
-   */
-  $(window).on('resize', function(){
-     redrawLines();
-  });
+  function getTree($el){
+    return $el.parents('[data-type="tree-root"]')
+  }
 
-  function redrawLines(){
-    setTimeout(function(){ // async to let all the dom changes to take effect
-      $('.line').remove();
-      $('.circle').remove();
-      var l = links.length, link;
-      while (l-- > 0){
-        link = links[l];
-        link && drawLine(getNode(link.firstId, 'left'), getNode(link.secondId, 'right'), true);
-      }
-    })
+  function id($tree, $el){
+    return $tree.attr('id').replace('tree-root_','') + ':' + $el.data('id')
   }
 
   /**
    * Get the node corresponding to the treeId and node id
    * @param id
-   * @param treeId
+   * @param tabsetId
    * @returns {*|jQuery}
    */
-  function getNode(id, treeId){
-    return $('.tree[data-id="' + treeId + '"]').find('.angular-ui-tree-handle[data-id="' + id + '"]');
+  function getNode(id, tabsetId){
+    var ids = id.split(':'),
+        tree = getTreeFromId(id, tabsetId),
+        node = tree.find('[data-id="'+ ids[1] +'"]')
+    return node;
+  }
+
+  function getTreeFromId(id, tabsetId){
+    var ids = id.split(':'),
+        tree = $('#' + tabsetId + '_tabset').find('#tree-root_'+ ids[0]);
+    return tree;
   }
 
   /**
    * Add the link between items to the cache
-   * @param first item
-   * @param second item
+   * @param link link
    */
-  function addLink(first, second){
-    links.push({
-      first: first,
-      firstId: first.data('id'),
-      second: second,
-      secondId: second.data('id')
-    });
-//    alert('Connecting id1: ' + first.data('id') + ' and id2: ' + second.data('id'));
+  function addLink(link){
+    links.push(link);
+    console.log('Connecting node [' + link.firstId + '] and ['+ link.secondId + ']');
   }
 
   /**
    * Find if there is a link between two items and return it
-   * @param first item
-   * @param second item
+   * @param firstId
+   * @param secondId
    * @returns {*} a link
    */
-  function findLink(first, second){
+  function findLink(firstId, secondId){
     var l = links.length, link;
     while (l-- > 0){
       link = links[l];
-      if (link && link.first.data('id') == first.data('id') && link.second.data('id') == second.data('id')){
+      if (link && link.firstId == firstId && link.secondId == secondId){
         return link;
       }
     }
@@ -195,14 +278,14 @@
 
   /**
    * Removes every link that contains the element
-   * @param element
+   * @param id
    * @returns {*}
    */
-  function removeLinks(element){
+  function removeLinks(id){
     var l = links.length, link;
     while (l-- > 0){
       link = links[l];
-      if (link && (link.first.data('id') == element.data('id') || link.second.data('id') == element.data('id'))){
+      if (link && (link.firstId == id || link.secondId == id)){
         links[l] = null;
       }
     }
@@ -212,7 +295,7 @@
    * Draw a line between two elements with two dots at the ends
    * @param _1 first jqLite/jquery element
    * @param _2 second jqLite/jquery element
-   * @param jq - true if elements are jquery elements
+   * @param jq - true if elements are jquery elements and false if jqLite elements
    */
   function drawLine(_1, _2, jq){
     var el1 = getElement(_1, jq),
